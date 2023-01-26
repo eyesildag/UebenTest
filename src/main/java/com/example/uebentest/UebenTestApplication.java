8e4d53529c8f8b71a5be9070930b9a2399ea6b1b
Emre Yesildag <e12120490@student.tuwien.ac.at>
2023-01-26 12:29:00 +0100

    Initial commit

---
+++ src/main/java/com/example/uebentest/UebenTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.uebentest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UebenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UebenTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uebentest/UebenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uebentest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UebenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
